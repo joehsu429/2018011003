f585d6b63a57738d8290ab80fa3d105bf9fd49f0
joe <kkwhsu@gmail.com>
2018-01-10 14:00:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a2018011003/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.student.a2018011003;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    Handler handler= new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.d("MSG","取得一個Message");
+            Bundle b=msg.getData();
+            String str=b.getString("data");
+            Log.d("MSG","data"+str);
+        }
+    };
+    public void click1(View v)
+    {
+           Message msg = new Message();
+           Bundle b= new Bundle();
+           b.putString("data","Mydata");
+           msg.setData(b);
+           handler.sendMessageDelayed(msg,3000);
+    }
+//        Handler handler=new Handler(){
+//            @Override
+//            public void handleMessage(Message msg) {
+//                super.handleMessage(msg);
+//                Log.d("MSG","取得一個Message");
+//            }
+//        };
+//        public void click1(View v)
+//        {
+//            Message msg=new Message();
+//            handler.sendMessage(msg);
+//        }
+    }
+
